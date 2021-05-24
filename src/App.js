b6ec543a44419840290b62c433fb38b5a06db0f7
janiapurva <jani.apurva@yahoo.com>
2021-05-24 17:26:26 -0400

    create global syle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import styled from "styled-components";
 function App() {
   return (
-    <AppStyled>
+    <div>
       <h1>STYLED COMPONENT</h1>
-    </AppStyled>
+    </div>
   );
 }
 
-const AppStyled = styled.div`
-background-color:red;
 
-`;
+
+
 export default App;
