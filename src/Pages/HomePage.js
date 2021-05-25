7fb30c4963120fd377eff958ff655426238cce16
janiapurva <jani.apurva@yahoo.com>
2021-05-24 23:39:09 -0400

    Display icon on main page

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,6 +1,9 @@
 import React from 'react'
 import styled from "styled-components";
 import Particle from '../Components/Particles';
+import TwitterIcon from '@material-ui/icons/Twitter'
+import GithubIcon from '@material-ui/icons/GitHub'
+import LinkedinIcon from '@material-ui/icons/LinkedIn'
 
 function HomePage() {
   return (
@@ -8,7 +11,21 @@ function HomePage() {
       <div className="p-particles-js">
         <Particle/>
       </div>
+      <div className="typography">
+      <h1>Hi I am Apurva Jani<span>
+        Loream Ipsam</span></h1>
+        <p>
+          loream 20 dollar sananslkdnfansflanflnannfsfnsalk
+        </p>
+        <div className="icons">
+          <div className="i-twitter"><TwitterIcon/></div>
+          <div className="i-github"><GithubIcon/></div>
+          <div className="i-linkedin"><LinkedinIcon/></div>
+
+        </div>
+    </div>
     </HomePageStyled>
+    
   )
 }
 
@@ -16,6 +33,11 @@ const HomePageStyled = styled.header`
 width: 100%;
 height: 100vh;
 position: relative;
+.p-particles-js{
+  position: absolute;
+  top:0;
+  left: 0;
+}
 
 
 `
