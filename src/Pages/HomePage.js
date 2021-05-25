51fffdbabf29ef7619a6e9af47ef26a280c9be7a
janiapurva <jani.apurva@yahoo.com>
2021-05-25 15:02:52 -0400

    create and styled social media link

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -1,44 +1,83 @@
-import React from 'react'
+import React from "react";
 import styled from "styled-components";
-import Particle from '../Components/Particles';
-import TwitterIcon from '@material-ui/icons/Twitter'
-import GithubIcon from '@material-ui/icons/GitHub'
-import LinkedinIcon from '@material-ui/icons/LinkedIn'
+import Particle from "../Components/Particles";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import GithubIcon from "@material-ui/icons/GitHub";
+import LinkedinIcon from "@material-ui/icons/LinkedIn";
 
 function HomePage() {
   return (
     <HomePageStyled>
       <div className="p-particles-js">
-        <Particle/>
+        <Particle />
       </div>
       <div className="typography">
-      <h1>Hi I am Apurva Jani<span>
-        Loream Ipsam</span></h1>
+        <h1>
+          Hi I'm <span>Apurva Jani</span>
+        </h1>
         <p>
-          loream 20 dollar sananslkdnfansflanflnannfsfnsalk
+          Full stack developer with extensive background in various coding
+          languages. 
         </p>
         <div className="icons">
-          <div className="i-twitter"><TwitterIcon/></div>
-          <div className="i-github"><GithubIcon/></div>
-          <div className="i-linkedin"><LinkedinIcon/></div>
-
+          <a href="https://twitter.com/janiapurva" className="icon i-twitter">
+            <TwitterIcon />
+          </a>
+          <a href="https://github.com/janiapurva" className="icon i-github">
+            <GithubIcon />
+          </a>
+          <a href="https://www.linkedin.com/in/apurva-jani-19b72428/" className="icon i-linkedin">
+            <LinkedinIcon />
+          </a>
         </div>
-    </div>
+      </div>
     </HomePageStyled>
-    
-  )
+  );
 }
 
 const HomePageStyled = styled.header`
-width: 100%;
-height: 100vh;
-position: relative;
-.p-particles-js{
-  position: absolute;
-  top:0;
-  left: 0;
+  width: 100%;
+  height: 100vh;
+  position: relative;
+  .p-particles-js {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+  .typography {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    text-align: center;
+    width: 80%;
+    
+    .icons{
+      display: flex;
+      justify-content: center;
+      margin-top:1rem;
+      .icon{
+        border:2px solid var(--border-color);
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 50%;
+        transition: all .4s ease-in-out;
+        cursor: pointer;
+
+        &:hover{
+          border:2px solid var(--primary-color);
+          color: var(--primary-color);
+        }
+        &:not(:last-child){
+          margin-right: 1rem;
+        }
+svg{
+  margin: .5rem;
 }
+      }
 
-
-`
-export default HomePage
+    }
+  }
+`;
+export default HomePage;
--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -34,6 +34,16 @@ a{
   color: inherit;
   font-size: inherit;
 }
+h1{
+  font-size: 4rem;
+  color: var(--white-color);
+  span{
+    font-size: 4rem;
+  }
+}
+span{
+  color: var(--primary-color);
+}
 
 `;
 
