3a40f3af7aa6611b05a3196536753ca631235d7b
janiapurva <jani.apurva@yahoo.com>
2021-06-02 22:43:15 -0400

    moved toggle up from website

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -123,7 +123,7 @@ h6{
 .light-dark-mode{
     position: fixed;
     right: 0;
-    top: 50%;
+    top: 22%;
     background-color: var(--background-light-color-2);
     width: 6.5rem;
     height: 2.5rem;
